d2f7ee145b796d880fbce5843f1b417ac201902b
NCLnclNCL <nanvanu@gmail.com>
2025-07-30 14:40:57 +0700

    Revert "fix driver a46"
    This reverts commit d255123c1b8672e9d3739b47ba0f387134c6785d.

--- drivers/misc/mediatek/imgsensor/src/mt6765/camera_hw/imgsensor_cfg_table.c
+++ drivers/misc/mediatek/imgsensor/src/mt6765/camera_hw/imgsensor_cfg_table.c
@@ -164,20 +164,6 @@ struct IMGSENSOR_HW_POWER_SEQ sensor_power_sequence[] = {
         },
     },
 #endif
-#if defined(GC2375H_CHENGXIANGTONG_MIPI_RAW)
-    {
-        SENSOR_DRVNAME_GC2375H_CHENGXIANGTONG_MIPI_RAW,
-        {
-            {PDN, Vol_High, 0, Vol_Low, 0},
-            {DOVDD, Vol_1800, 1, Vol_Low, 0},
-            {AVDD, Vol_2800, 1},
-            {SensorMCLK, Vol_High, 0},
-            {RST, Vol_Low, 1, Vol_Low, 0},
-            {PDN, Vol_Low, 0, Vol_High, 0},
-            {RST, Vol_High, 0, Vol_High, 0}
-        },
-    },
-#endif
 #if defined(GC8034_MIPI_RAW)
     {
         SENSOR_DRVNAME_GC8034_MIPI_RAW,
